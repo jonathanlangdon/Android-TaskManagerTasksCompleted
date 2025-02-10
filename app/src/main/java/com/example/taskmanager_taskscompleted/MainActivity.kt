7072533712db3d3a024807ad8417dffa262c8624
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-02-10 15:54:25 -0500

    image and text added

--- app/src/main/java/com/example/taskmanager_taskscompleted/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager_taskscompleted/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager_taskscompleted.ui.theme.TaskManagerTasksCompletedTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +44,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TasksCompletedPage(title: String, encourage: String, modifier: Modifier = Modifier) {
+fun PageText(title: String, encourage: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+        )
+        Text(
+            text = encourage,
+            fontSize = 16.sp,
+            modifier = Modifier
+        )
+    }
+}
 
+@Composable
+fun TasksCompletedPage(title: String, encourage: String, modifier: Modifier = Modifier) {
+    val checkMarkImage = painterResource(R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+    ) {
+        Image(
+            painter = checkMarkImage,
+            contentDescription = null,
+            contentScale = ContentScale.None
+        )
+        PageText(
+            title = title, encourage = encourage, modifier = Modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
